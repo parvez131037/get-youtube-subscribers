f40166b4e79d49e04c634e5e4645c6e00d32f5e8
parvez131037 <parvezahmed.ansari45@gmail.com>
2022-11-29 20:26:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+//Import the express module
+
+const express = require('express')
+
+//import Routes from app.js
+const app = require('./app.js')
+
+//import mongoose module
+const mongoose = require('mongoose')
+
+//Declare a specific listening port number
+// const port = 3000
+const PORT = process.env.PORT || 3000;
+
+// Wide listing a cors to accept a specific domain route
+const cors = require('cors')
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+//enable cors usage
+app.use(cors());
+
+// Connect to DATABASE
+// const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = "mongodb://0.0.0.0/subscribers";
+mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection
+db.on('error', (err) => console.log(err))
+db.once('open', () => console.log('connected to database'))
+
+//app.listen() function which Binds and listens for connections on the specified host and port.
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+
+
+//for testing
+module.exports = app;
