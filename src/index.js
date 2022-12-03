8611d685300110b0ac9c953a72fd7e69291a96fa
Parvez Ansari <106295429+parvez131037@users.noreply.github.com>
2022-12-03 18:32:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,12 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 //enable cors usage
-app.use(
-  cors({
-    origin: “*”,
-    credentials: true
-  })
-)
+app.use(cors());
 
 // Connect to DATABASE
 // const DATABASE_URL = "mongodb://localhost/subscribers";
